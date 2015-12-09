368e7ebded13537389e1ae5499ad64f08cd25929
weekheongquape <wee.kheong@quape.com>
2015-12-09 22:14:56 +0800

    ALL

--- www/js/notify.js
+++ www/js/notify.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		app.init();
+		//app.init();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
